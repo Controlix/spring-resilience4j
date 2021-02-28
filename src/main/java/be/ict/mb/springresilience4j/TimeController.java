c62d0e6d25c62af483eae76f37dadc6187570493
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2021-02-28 18:13:35 +0100

    Provide fallback for time

--- src/main/java/be/ict/mb/springresilience4j/TimeController.java
+++ src/main/java/be/ict/mb/springresilience4j/TimeController.java
@@ -1,11 +1,15 @@
 package be.ict.mb.springresilience4j;
 
+import io.github.resilience4j.decorators.Decorators;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.function.Supplier;
 
 @Slf4j
@@ -19,9 +23,21 @@ public class TimeController {
     @GetMapping("/raw")
     public String justGetTheTime() {
         TimeMessage time = timeSupplier.get();
-        log.debug("Received %s", time);
+        log.debug("Received {}", time);
 
         return time.getTime();
     }
 
+    @GetMapping("/default")
+    public String uponFailureUseGMTTime() {
+        TimeMessage time = Decorators.ofSupplier(timeSupplier)
+                .withFallback(e -> {
+                    log.warn("Could not obtian time, fallback to UTC time");
+                    return new TimeMessage(LocalDateTime.now(Clock.systemUTC()).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+                })
+                .get();
+        log.debug("Received {}", time);
+
+        return time.getTime();
+    }
 }
